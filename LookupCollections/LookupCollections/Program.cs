d52ce3d1305d731bfc9e1b655f543912c9c83ca1
Gaál Szilárd <gaalszilard23@gmail.com>
2019-06-20 11:36:43 +0200

    all tasks complete

---
+++ LookupCollections/LookupCollections/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Globalization;
+
+namespace LookupCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new ListDictionary(new CaseInsensitiveComparer(CultureInfo.InvariantCulture))
+            {
+                ["Estados Unidos"] = "United States of America",
+                ["Canadá"] = "Canada",
+                ["España"] = "Spain"
+            };
+
+            Console.WriteLine(dict["CANADÁ"]);
+            Console.WriteLine(dict["españa"]);
+            Console.ReadLine();
+        }
+    }
+}

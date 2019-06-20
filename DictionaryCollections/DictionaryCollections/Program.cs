754f7286767a56aae3b2cfc59d32579a5f1a712e
Gaál Szilárd <gaalszilard23@gmail.com>
2019-06-20 11:35:49 +0200

    tasks complete

---
+++ DictionaryCollections/DictionaryCollections/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace DictionaryCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hTable = new Hashtable();
+            string[] numStr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+            for (int i = 0; i < 10; i++)
+            {
+                hTable.Add(i.ToString(), numStr[i]);
+            }
+
+            string str = "2304535356794";
+
+            foreach (char c in str) {
+                string num = c.ToString();
+                if (hTable.ContainsKey(num))
+                {
+                    Console.WriteLine(hTable[num]);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

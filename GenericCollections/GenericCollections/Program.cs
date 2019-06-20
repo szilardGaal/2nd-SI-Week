019c99662fc9c482a3692a269bb89f442a581044
Gaál Szilárd <gaalszilard23@gmail.com>
2019-06-20 11:36:14 +0200

    tasks complete

---
+++ GenericCollections/GenericCollections/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<int, string>();
+
+            dict[36] = "Hungary";
+            dict[44] = "United Kingdom";
+            dict[33] = "France";
+            dict[55] = "Brazil";
+            
+            foreach (KeyValuePair<int, string> item in dict)
+            {
+                int code = item.Key;
+                string country = item.Value;
+                Console.WriteLine("Code {0} = {1}", code, country);
+            }
+            Console.ReadLine();
+        }
+    }
+}

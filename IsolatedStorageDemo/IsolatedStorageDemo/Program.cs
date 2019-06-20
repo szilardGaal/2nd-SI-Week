37548d414474e23d1faf322afbba18fc0f5e7bbc
Gaál Szilárd <gaalszilard23@gmail.com>
2019-06-20 11:36:29 +0200

    all tasks complete

---
+++ IsolatedStorageDemo/IsolatedStorageDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.IsolatedStorage;
+using System.IO;
+
+namespace IsolatedStorageDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsolatedStorageFile userStore = IsolatedStorageFile.GetUserStoreForAssembly();
+            IsolatedStorageFileStream userStream = new IsolatedStorageFileStream("UserSettings.set", System.IO.FileMode.Create, userStore);
+            StreamWriter userWriter = new StreamWriter(userStream);
+            userWriter.WriteLine("User prefs");
+            userWriter.Close();
+
+            string[] Files = userStore.GetFileNames("UserSettings.set");
+            if (Files.Length == 0)
+            {
+                Console.WriteLine("file not found");
+                Console.ReadLine();
+                return;
+            }
+
+            userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Open, userStore);
+            StreamReader userReader = new StreamReader(userStream);
+            string contents = userReader.ReadToEnd();
+
+            Console.WriteLine(contents);
+            Console.ReadLine();
+        }
+    }
+}
